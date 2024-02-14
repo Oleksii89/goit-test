c5c2ac8dfc77b26b3097dde4fbb55de6b4fc2ee3
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2024-02-14 15:27:40 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
-import { Suspense, lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import SharedLayout from './components/SharedLayout/SharedLayout';
-import { Loader } from 'components/Loader/Loader';
+// import { Suspense, lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import SharedLayout from './components/SharedLayout/SharedLayout';
+// import { Loader } from 'components/Loader/Loader';
+import { Car } from 'components/Car/Car';
+import carsJson from '../src/cars';
 
-const HomePage = lazy(() => import('pages/HomePage/HomePage'));
-const MovieDetailsPage = lazy(() =>
-  import('pages/MovieDetailsPage/MovieDetailsPage')
-);
+const cars = carsJson;
+// const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+// const MovieDetailsPage = lazy(() =>
+//   import('pages/MovieDetailsPage/MovieDetailsPage')
+// );
 
-const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+// const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
 
 export const App = () => {
+  console.log(cars);
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+    <ul>
+      {cars.map(car => {
+        return (
+          <Car
+            key={car.id}
+            model={car.model}
+            make={car.make}
+            year={car.year}
+            img={car.img}
+            price={car.rentalPrice}
+            favourite={false}
+          />
+        );
+      })}
+    </ul>
+    // <Suspense fallback={<Loader />}>
+    //   <Routes>
+    //     <Route path="/" element={<SharedLayout />}>
+    //       <Route index element={<HomePage />} />
+    //       <Route path="/movies" element={<MoviesPage />} />
+    //       <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
 
-          <Route path="*" element={<HomePage />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    //       <Route path="*" element={<HomePage />} />
+    //     </Route>
+    //   </Routes>
+    // </Suspense>
   );
 };
---
+++ src/components/Car/Car.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import css from './Car.module.css';
+import { ReactComponent as IconHeart } from '../../assets/images/heart.svg';
+
+export const Car = ({ year, make, model, price, favourite, img, id }) => {
+  return (
+    <li>
+      <div>
+        <img className={css.img} src={img} alt={model} />
+        <button>
+          <IconHeart
+            className={`${css.IconHeart} ${css.favourite ? css.favourite : ''}`}
+          />
+        </button>
+      </div>
+      <p>
+        <b>{make}</b>
+      </p>
+      <p>
+        <b>{model}</b>,
+      </p>
+      <p>
+        <b>{year}</b>
+      </p>
+      <p>
+        <b>{price}</b>
+      </p>
+    </li>
+  );
+};
